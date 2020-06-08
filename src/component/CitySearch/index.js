0de91b3a54592858cdc42117420b3bf561e5bbd0
Jacob Stanger <jstangwork@gmail.com>
2020-06-08 09:01:34 -0600

    onchange and onsubmit

--- src/component/CitySearch/index.js
+++ src/component/CitySearch/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import './index.css'
+import React, { useState } from 'react';
+import './index.css';
+
+
+
+
+const cl = (text) => {
+  return console.log(text);
+};
 
 const CitySearch = () => {
+  const [cityInput, setCityInput] = useState('');
+  
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    cl(`ready to search for ${cityInput}`);
+  };
+
+  const onChangeHandler = (event) => {
+    const userInputSearchString = event.target.value;
+    setCityInput(userInputSearchString);
+  };
+
   return (
     <div className="container city-search">
-      <div className="field has-addons">
+      <div className="field has-addons" >
         <div className="control">
-          <input className="input" type="text" placeholder="City Name" />
+          <input 
+            className="input" 
+            type="text" 
+            placeholder="City Name" 
+            onChange={onChangeHandler}
+          />
         </div>
-        <div className="control">
+        <div className="control" onClick={onSubmitHandler}>
           <a className="button is-info" href="/">
             <i className="fas fa-location-arrow"></i>
           </a>
