c6e12bbd51236650eab9b0ee3f4bdb4751926b65
Jacob Stanger <jstangwork@gmail.com>
2020-06-08 14:15:22 -0600

    code getting hard to read need to refactor

--- src/App.js
+++ src/App.js
@@ -11,52 +11,45 @@ import axios from 'axios';
 function App() {
 
   const [citySearchString, setCitySearchString] = useState('Chicago');
+  const [latitude, setLatitude] = useState('33.441792');
+  const [longitude, setLongitude] = useState('-94.037689');
+  const [weatherData, setWeatherData] = useState([]);
   const apiKey = 'a41553bf7961d05765a23fa436102cf6';
+
+  // const getForecastString = `https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=minutely&appid=${apiKey}`;
+  // const queryString = `https://api.openweathermap.org/data/2.5/weather?q=${citySearchString}&appid=${apiKey}&units=imperial`;
+
+  const getLatAndLonFromString = async () => {
+    let queryStr = `https://api.openweathermap.org/data/2.5/weather?q=${citySearchString}&appid=${apiKey}&units=imperial`;
+    let response = await fetch(queryStr);
+    let data = await response.json();
+    // await setLatitude(data.coord.lat);
+    // await setLongitude(data.coord.lon);
+    return data.coord;
+  };
+
+  const getForecastFromCoordinates = async (coordinates) => {
+    const latitude = coordinates.lat;
+    const longitude = coordinates.lon;
+
+    const getForecastString = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=${apiKey}`;
+    let response = await fetch(getForecastString);
+    let data = await response.json();
+    return data;
+  };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    const queryString = `https://api.openweathermap.org/data/2.5/weather?q=${citySearchString}&appid=${apiKey}&units=imperial`;
     console.log(`ready to search for ${citySearchString}`);
-    console.log(queryString);
+    // console.log(queryString);
+    setWeatherData(
+      getForecastFromCoordinates(
+        getLatAndLonFromString()
+      )
+    );
 
-    // let requestOptions = {
-    //   method: 'GET',
-    //   redirect: 'follow'
-    // };
-    
-    // fetch("api.openweathermap.org/data/2.5/weather?q=roy&appid=a41553bf7961d05765a23fa436102cf6", requestOptions)
-    //   .then(response => response.json())
-    //   .then(result => console.log(result))
-    //   .catch(error => console.log('error', error));
-
-    // axios.get(queryString)
-    //   .then((data) => {
-    //     console.log(data);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-
-    fetch(queryString)
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
-        console.log(data.main.temp);
-        console.log(data.coord);
-      });
-
-    // fetch(queryString)
-    //   .then((weatherResults) => {
-    //     console.log(weatherResults.body);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-
-    // fetch(queryString)
-    //   .then(response => response.json())
-    //   .then(data => console.log(data));
   }
+
 
   const onChangeHelper = (event) => {
     const userInputSearchString = event.target.value;
@@ -72,6 +65,7 @@ function App() {
           </a>
         </div>
       </nav>
+      <h1>{weatherData}</h1>
       <CitySearch 
         onChangeHandler={onChangeHelper}
         onSubmitHandler={submitHandler}
