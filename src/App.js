d6d802cc853e05c9c7e50e94bf86013356ab832e
Jacob Stanger <jstangwork@gmail.com>
2020-06-09 13:46:46 -0600

    seperate useStates

--- src/App.js
+++ src/App.js
@@ -14,24 +14,26 @@ const createImageString = (imgCode) => {
 function App() {
 
   const iconId = '10d';
-  const sampleIconUrl = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
-  const [currentTemp, setCurrentTemp] = useState(69);
-  const [imageFileName, setImageFileName] = useState(sampleIconUrl);
-  
+  // const sampleIconUrl = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
+  const [currentTemp, setCurrentTemp] = useState(0);
+  const [imageFileName, setImageFileName] = useState(createImageString(iconId));
+  const [dayOfTheWeek, setDayOfTheWeek] = useState('Sunday');
+  const [highTemp, setHighTemp] = useState(0);
+  const [lowTemp, setLowTemp] = useState(0);
+
   const [citySearchString, setCitySearchString] = useState('Chicago');
   const apiKey = 'a41553bf7961d05765a23fa436102cf6';
   const queryString = `https://api.openweathermap.org/data/2.5/weather?q=${citySearchString}&appid=${apiKey}&units=imperial`;
 
-  const [weatherDataList, setWeatherDataList] = useState([
-    {
-      currTemp: 1,
-      imageFileName: createImageString(iconId),
-      day: "Sunday",
-      highTemp: 90,
-      lowTemp: 40
-    }
-  ]);
-
+  // const [weatherDataList, setWeatherDataList] = useState([
+  //   {
+  //     currTemp: 1,
+  //     imageFileName: createImageString(iconId),
+  //     day: "Sunday",
+  //     highTemp: 90,
+  //     lowTemp: 40
+  //   }
+  // ]);
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -48,10 +50,13 @@ function App() {
           // setCurrentTemp(data.main.temp);
 
           //copy data then mutate it
-          const copyWeather = weatherDataList[0];
-          copyWeather.currTemp = data.main.temp;
-          
-          setWeatherDataList(weatherDataList => [...weatherDataList, copyWeather]);
+          // const copyWeather = weatherDataList[0];
+          // copyWeather.currTemp = data.main.temp;
+          // setWeatherDataList(weatherDataList => [...weatherDataList, copyWeather]);
+          setCurrentTemp(data.main.temp);
+          setHighTemp(data.main.temp_max);
+          setLowTemp(data.main.temp_min);
+
         } catch(err) {
           console.log(err);
         }
@@ -81,7 +86,18 @@ function App() {
         onSubmitHandler={submitHandler}
       />
       <WeatherContainer data={mockWeather.data} />
-      <WeatherContainer data={weatherDataList} />
+      {/* <WeatherContainer data={weatherDataList} /> */}
+      <WeatherContainer 
+        data={[
+          {
+            day: dayOfTheWeek,
+            imageFileName: imageFileName,
+            currTemp: currentTemp,
+            highTemp: highTemp,
+            lowTemp: lowTemp
+          }
+        ]} 
+      />
     </div>
   );
 }
